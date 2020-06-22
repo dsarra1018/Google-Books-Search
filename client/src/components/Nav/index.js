c02e8f043c604c1af6d69241ef91c69e54bfd510
Darren Sarra <adriansarra18@gmail.com>
2020-06-21 21:19:06 -0400

    Started a Nav component.

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./style.css";
+
+function Nav() {
+  return(
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <span>Google Books</span>
+
+      <div className="nav-items">
+        <Link className="links" to="/">Search</Link> 
+        <Link className="links" to="/saved">Saved</Link>
+      </div>
+      
+    </nav>
+  );
+}
+
+export default Nav;

d74a8f93c0ff983c7f48499fb5e81493c4fac17e
Darren Sarra <adriansarra18@gmail.com>
2020-06-22 01:52:08 -0400

    Started api routes.

---
+++ routes/api-routes.js
@@ -0,0 +1,52 @@
+const axios = require("axios");
+const db = require("../models");
+const path = require("path");
+
+module.exports = function(app) {
+  app.get("/api/books", (req, res) => {
+    db.Book.find().then(
+      (booksData) => {
+        res.json(booksData);
+      }
+    ).catch( err => {
+      res.status(400).json(err);
+    });
+  });
+
+  app.post("/api/books", (req, res) => {
+    let title = req.body.title.replace(/\s/g, "+");
+    axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${title}&key=${process.env.KEY}`
+    ).then(
+      (response) => {
+        res.json(response.data.items)
+      }
+    ).catch( err => {
+      res.status(400).json(err);
+    });
+  });
+
+  app.post("/api/books", (req, res) => {
+    db.Book.create(req.body).then(
+      response => {
+        res.json({successful:response});
+      }
+    ).catch( err => {
+      res.status(400).json(err);
+    });
+  });
+
+  app.delete("/api/books/:id", (req, res) => {
+    db.Book.findByIdAndDelete(req.params.id).then( 
+      response => {
+        res.json({successful:response});
+      }
+    ).catch( err => {
+      res.status(400).json(err);
+    });
+  });
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}

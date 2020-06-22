6af113557a3ba33aa43a930a586cfa55977b0fe8
Darren Sarra <adriansarra18@gmail.com>
2020-06-21 21:31:29 -0400

    Added jumbotron component in the saved page.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Jumbotron from "../components/Jumbotron";
 
 function Saved() {
   return(
     <div>
       <Nav />
+      <Jumbotron />
     </div>
   )
 }

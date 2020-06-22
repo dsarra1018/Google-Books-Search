d319252a2c161b63f8f95dc207a3c728e4c046bf
Darren Sarra <adriansarra18@gmail.com>
2020-06-21 21:31:49 -0400

    Added jumbotron component in the search page.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Jumbotron from "../components/Jumbotron";
 
 function Search() {
   return(
     <div>
       <Nav />
+      <Jumbotron />
     </div>
   );
 }

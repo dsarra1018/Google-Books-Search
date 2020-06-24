fcbf733ca0cc1b964affa288f3d9e3b181493011
Darren Sarra <adriansarra18@gmail.com>
2020-06-24 16:09:36 -0400

    Added a search form inside the search pages.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Jumbotron from "../components/Jumbotron";
+import Search from "../components/Form";
 
 function Search() {
   return(
     <div>
       <Nav />
       <Jumbotron />
+      <Search />
     </div>
   );
 }

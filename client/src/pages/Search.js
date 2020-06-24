ea350017628e146d4bf9bf8598cad910d42387f6
Darren Sarra <adriansarra18@gmail.com>
2020-06-24 17:27:35 -0400

    handles states.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,16 +1,42 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Jumbotron from "../components/Jumbotron";
-import Search from "../components/Form";
+import SearchForm from "../components/Form";
+import API from "../utils/API";
 
-function Search() {
-  return(
-    <div>
-      <Nav />
-      <Jumbotron />
-      <Search />
-    </div>
-  );
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      book_input: "",
+      bookData: []
+    }
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({book_input: e.target.value})
+  }
+
+  handleSearchClick(e) {
+    e.preventDefault();
+    API.getBooks(this.state.book_input)
+      .then(res => {
+        this.setState({bookData: res.data});
+        this.setState({book_input: ""});
+      });
+  }
+  render() {
+    return(
+      <div>
+        <Nav />
+        <Jumbotron />
+        <SearchForm handleChange={this.handleChange} handleSearchClick={this.handleSearchClick} />
+      </div>
+    );
+  }
 }
 
 export default Search;

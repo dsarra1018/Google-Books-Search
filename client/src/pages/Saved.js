807a8d19f665191e8599d7b232cc591cc0313688
Darren Sarra <adriansarra18@gmail.com>
2020-06-27 00:39:32 -0400

    Added utils/API into saved pages.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Nav from "../components/Nav";
 import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
 
-function Saved() {
-  return(
-    <div>
-      <Nav />
-      <Jumbotron />
-    </div>
-  )
+class Saved extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      savedBooks: []
+    }
+  }
+
+  componentWillMount() {
+    API.getBooks().then(res => {
+      this.setState({savedBooks: res.data});
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <Nav />
+        <Jumbotron />
+      </div>
+    )};
 }
 
 export default Saved;

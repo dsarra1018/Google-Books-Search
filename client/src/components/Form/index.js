b45bf253afac7453988113e2e7d26cf5b43f28af
Darren Sarra <adriansarra18@gmail.com>
2020-06-27 00:37:47 -0400

    Created a search form and added intothe search page.

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import "./style.css";
 
 function Search(props) {
   return(
-    <div>
-      <h4>Book Search</h4>
+    <div className="search-div">
+      <h4 className="search-head">Book Search</h4>
       <form className="book-search">
-        <label htmlFor="book-input" form="search-form">Book:</label>
-        <input type="text" name="book-input" form="book-search" onChange={(e) => props.handleChange(e)} placeholder="Enter a book title" required></input>
-        <button type="submit" onClick={(e) => props.handleSearchClick(e)}>Search</button>
+        <label htmlFor="book_input" form="book-search">Book:</label>
+        
+        <br></br>
+        <span>
+          <input type="text" name="book_input" id="book_input" form="book-search" onChange={(e) => props.handleChange(e)} placeholder="Enter a book title"/>
+        </span>
+        
+        <br></br>
+        <button type="submit" className="submit-btn" onClick={(e) => props.handleSearchClick(e)}>Search</button>
       </form>
     </div>
   );

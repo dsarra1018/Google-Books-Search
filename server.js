cf334b781faf3c92b3b015433b86970e9319a499
Darren Sarra <adriansarra18@gmail.com>
2020-06-28 17:56:28 -0400

    Fixed server.js.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/api-routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 

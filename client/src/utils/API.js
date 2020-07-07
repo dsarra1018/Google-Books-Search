c9eada790f75afe793c6ef9e0cd3e2df7ea081e9
Darren Sarra <adriansarra18@gmail.com>
2020-07-07 01:24:23 -0400

    Fixed routing issues.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,18 +2,19 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getBooks: () => {
+    return axios.get("/api/books")
   },
-  // Gets the book with the given id
-  getBook: function(title) {
-    return axios.get("/api/books/", {title: title});
+  // Gets books with the given title
+  searchBooks: (title) => {
+    return axios.post("/search", {title: title});
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  saveBook: function(bookData) {
+  // Saves books
+  saveBook: (bookData) => {
     return axios.post("/api/books", bookData);
+  },
+  // Deletes books
+  deleteBook: (id) => {
+    return axios.delete(`/api/books/${id}`);
   }
-};
+}

ecea69465d54592cb7683a92fdf37d248142b28c
Darren Sarra <adriansarra18@gmail.com>
2020-07-07 01:25:13 -0400

    Fixed server.js file.

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ if(process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to mongoDB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect((process.env.MONGODB_URI || "mongodb://localhost/googlebooks"), {useNewUrlParser: true})
+  .then(() => {
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  }).catch((err) => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
 
 // Start the API server
 app.listen(PORT, function() {
